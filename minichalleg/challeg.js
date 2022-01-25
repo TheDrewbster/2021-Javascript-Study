26b79078e110f96afbd9dfa8a8139f98815f9b66
Drew Fischer <drew.fischer@vtsd.com>
2022-01-25 09:24:38 -0500

    12/15/21

---
+++ minichalleg/challeg.js
@@ -0,0 +1,11 @@
+/*
+Design a page that when you change a text input, an image gets bigger.
+ */
+function bigBig(){
+    var image = document.getElementById("meme");
+    image.style.width = "115%";
+}
+function showButton(){
+    var text = document.getElementById("text");
+    button.style.display = "block";
+}
